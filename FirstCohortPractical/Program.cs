816a05765d499e7a315ba52b50a9eb250d1c6397
Arike115 <ajibolaoluwafunke669@gmail.com>
2023-10-14 18:51:22 +0100

    Add project files.

---
+++ FirstCohortPractical/Program.cs
@@ -0,0 +1,49 @@
+﻿public class Program //class
+{ //opening of a class
+
+    //Datatypes
+    //string
+    //interger int
+    //decimal
+    //float
+    //long
+    //double
+    //bool
+    //short
+    //byte
+    //string
+    //how to declare a datatype
+
+    static string Words = "chalk-board";
+    static int Number = 45362483;
+    decimal value = 127.6453m;
+    double v = 21632867394674764.557;
+    bool y = true;
+    float x = 374526664234742671423534678438683.7f;
+    long h = 219236754954239512;
+    byte o = 217;
+    short k = 7823;
+
+    //array
+    static string[] Data = { "wardrobe", "carpet", "chalk", "plate", "cup" };
+    int[] Numerical = { 675, 894, 464, 46737, 7463 };
+    private static void Main(string[] args) //method
+    {//opening of your method
+        //{} curly braces
+
+        //statement
+        Console.WriteLine(Words);
+        Console.WriteLine(Number);
+        Console.WriteLine(999);
+
+        //how to print an array
+        foreach (string  s in Data) 
+        {
+            Console.WriteLine(s);
+        }
+
+
+    }//closing of your method
+   
+   
+}//closing of the class
