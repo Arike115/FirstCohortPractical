9c51920fccdcd8a65b8dfec4e357f47c9196aaef
Arike115 <ajibolaoluwafunke669@gmail.com>
2023-11-04 16:39:07 +0100

    changes

--- FirstCohortPractical/Program.cs
+++ FirstCohortPractical/Program.cs
@@ -1,46 +1,19 @@
-﻿public class Program //class
+﻿using FirstCohortPractical;
+
+public class Program //class
 {
-    //Statements
-    //Conditional statement, if else, switch case
-    //conditional looping, for, foreach, while, dowhile
+    //access modifier
 
     private static void Main(string[] args) //method
     {
-        decimal[] values = {12,90.8m,78,65.7m,89.90m,764.0m,89 }; 
-
-        ////Foreach
-        //foreach(decimal data in values)
-        //{
-        //    Console.WriteLine(data);
-        //}
-
-        //for
-      //field declaration
-        for(int v = 10; v >= 0; v-- )
-        {
-            Console.WriteLine(v);
-        }
-        //dowhile
-
-        int i = 0;
-        do
-        {
-            Console.WriteLine("value of i =" + i);
-            i++;
-        }while (i < values.Length);
-
-        //while
-        int x = 0; //declaration
-        while (x < values.Length) //setting condition
-        {
-            Console.WriteLine("value of x =" + x);
-            x++; //increment 
-        }
+        Students.StudentName();
+        Console.WriteLine(Students.v);
+        
     }
 
-   
 
-    
-   
-   
+
+
+
+
 }
---
+++ FirstCohortPractical/Students.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCohortPractical
+{
+  //accessmodifier
+  //public
+  //private
+  //internal
+  //protected
+    class Students
+    {
+       public static int v = 89; //field
+
+        public static void StudentName()
+        {
+            Console.WriteLine("welcome to this new topic");
+            StudentList();
+        }
+
+        private static void StudentList()
+        {
+            string[] studentdate = { "chioma", "Adanma", "Ngozie", "Ejiro", "Ben" };
+            foreach (string s in studentdate) 
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
