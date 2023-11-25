8501841d7139e5b24ac272bcca06f1689337a2bf
Arike115 <ajibolaoluwafunke669@gmail.com>
2023-11-25 16:40:15 +0100

    changes

--- FirstCohortPractical/Gender.cs
+++ FirstCohortPractical/Gender.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace FirstCohortPractical
 {
-    internal class DaysOfTheMonth
+    public enum Gender
     {
+        Male,
+        Female,
+        Unknown
     }
 }
