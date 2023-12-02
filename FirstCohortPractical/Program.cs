f46db59131a482bf1388f49660ba28834a39dfdc
Arike115 <ajibolaoluwafunke669@gmail.com>
2023-12-02 17:08:20 +0100

    changes

--- FirstCohortPractical/Program.cs
+++ FirstCohortPractical/Program.cs
@@ -1,53 +1,26 @@
 ﻿using FirstCohortPractical;
 using System.Linq.Expressions;
 
-public delegate void Hellodelegate(string message);
 public class Program //class
 {
-    //Delegate
-    //lamdba expression
-    //anonymous method
     //exception handling
+    //try
+    //catch
+    //finally
     private static void Main(string[] args) //method
     {
-        ////a delegate is a type safe function pointer
-        //Hellodelegate del = new Hellodelegate(Hello);
-        //del("welcome to delegate");
-
-        List<Teacher> techlist = new List<Teacher>();
-        techlist.Add(new Teacher() { TeacherId = 1, TeacherName = "Grace",TeacherSalary = 150000, Experience = 2});
-        techlist.Add(new Teacher() { TeacherId = 2, TeacherName = "Ben", TeacherSalary = 2000000, Experience = 3 });
-        techlist.Add(new Teacher() { TeacherId = 3, TeacherName = "Ruth", TeacherSalary = 170000, Experience = 4 });
-        techlist.Add(new Teacher() { TeacherId = 4, TeacherName = "John", TeacherSalary = 190000, Experience = 5 });
-        techlist.Add(new Teacher() { TeacherId = 5, TeacherName = "Lola", TeacherSalary = 100000, Experience = 7 });
-        techlist.Add(new Teacher() { TeacherId = 6, TeacherName = "Tayo", TeacherSalary = 150000, Experience = 8 });
-        techlist.Add(new Teacher() { TeacherId = 7, TeacherName = "Shola", TeacherSalary = 200000, Experience = 3 });
-        techlist.Add(new Teacher() { TeacherId = 8, TeacherName = "Levi", TeacherSalary = 120000, Experience = 9 });
-        techlist.Add(new Teacher() { TeacherId = 9, TeacherName = "Joshua", TeacherSalary = 150000, Experience = 2 });
-        techlist.Add(new Teacher() { TeacherId = 10, TeacherName = "ken", TeacherSalary = 190000, Experience = 1 });
-
-        //Ispromotable promo = new Ispromotable(promote);
-                                           // LambdaExpression
-        Teacher.PromotedTeacher(techlist, p => p.Experience >=5);
+        //array
+        try
+        {
+            int[] numbers = { 6, 5, 6, 7, 8 };
+            Console.WriteLine(numbers[11]);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
        
     }
 
-    //function
-    //public static bool promote (Teacher teacher)
-    //{
-    //   if(teacher.TeacherId >=6)
-    //    {
-    //        return true;
-    //    }
-    //    else
-    //    {
-    //        return false;
-    //    }
-    //}
-
-
-
-
-
-
 }
+ 
--- FirstCohortPractical/Teacher.cs
+++ FirstCohortPractical/Teacher.cs
@@ -6,28 +6,13 @@ using System.Threading.Tasks;
 
 namespace FirstCohortPractical
 {
-    //static class
-    //generic
-    public delegate bool Ispromotable(Teacher teacher); 
+
     public class Teacher
     {
         //properties
         public int TeacherId { get; set; }
         public string TeacherName { get; set; }
-        public decimal TeacherSalary { get; set; }   
+        public decimal TeacherSalary { get; set; }
         public int Experience { get; set; }
-
-        public static void PromotedTeacher(List<Teacher> teacherlist, Ispromotable isactive)
-        {
-            foreach(var teacher in teacherlist)
-            {
-                if(isactive(teacher))
-                {
-                    Console.WriteLine( teacher.TeacherName + "  is promoted");
-                }
-
-            }
-        }
-
     }
 }
