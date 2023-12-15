d0d552d59957f1c9a03fc41a9abe8d99e1502157
Arike115 <ajibolaoluwafunke669@gmail.com>
2023-12-15 16:37:40 +0100

    chnages

--- FirstCohortPractical/Program.cs
+++ FirstCohortPractical/Program.cs
@@ -10,70 +10,25 @@ public class Program
     //query syntax or query expression
     //method syntax or method extension or fluent syntax
     //Linqs have different methods group inside the different categories
-   //Element Operators
-   //Aggregate Operators
-   //Quantifiers ====> any, all, contains
+    //set operators
+    //ordering methods
+    //conversion method
 
     private static void Main(string[] args)
     {
-        //Quantifiers
-        string[] words = { "pen", "chalk", "duster", "desk", "jug", "mug" };
 
-        var anyresult = words.Any(s => s.Length > 4);
-        Console.WriteLine(anyresult);
+        double[] numbers1 = { 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.4, 2.1, 2.8 };
+        double[] numbers2 = { 2.9, 2.4, 2.2, 2.10, 2.11, 23, 22 };
 
-        var allresult = words.All(s => s.Length > 4);
-        Console.WriteLine(allresult);
+        var result = numbers1.Distinct().ToList();
+        var data = numbers2.Except(numbers1).ToList();
+        var intersect = numbers1.Intersect(numbers2).ToList();
+        var union = numbers1.Union(numbers2).ToList();
+        foreach (double number in union)
+        {
+            Console.WriteLine(number);
+        }
 
-        var contresult = words.Contains("chalk");
-        Console.WriteLine(contresult);
-
-        //aggregate operators
-        //sum
-        //min
-        //max
-        //count
-        //average
-        //aggregate
-
-        int[] numeric = { 45, 6, 7, 8, 2, 2, 4, 90, 89, 45, 0, 23 };
-
-        var sum = numeric.Sum();
-        Console.WriteLine("sum = " + sum);
-        var min = numeric.Min();
-        Console.WriteLine("minimum = " +min);
-        var max = numeric.Max();
-        Console.WriteLine("maximum =" + max);
-        var count = numeric.Count();
-        Console.WriteLine("Count = " + count);
-        var avarage = numeric.Average();
-        Console.WriteLine("avaerage = " +avarage);
-
-
-        //element Operators
-        //first
-        //firstordefault
-        //last
-        //lastordefault
-        //single
-        //singleordefault
-
-        int[] numerics = { 45, 6, 7, 8, 2, 2, 4, 90, 89, 45, 0, 23 };
-        int[] numerics2 = { };
-
-        var v = numerics.First();
-        Console.WriteLine(v);
-        var y = numerics.FirstOrDefault();
-        Console.WriteLine(y);
-        var i = numerics2.FirstOrDefault();
-        Console.WriteLine(i);
-
-        var o = numerics.Last();
-        Console.WriteLine(o);
-        var n = numerics.LastOrDefault();
-        Console.WriteLine(n);
-        var z = numerics2.LastOrDefault();
-        Console.WriteLine(z);
 
     }
 
