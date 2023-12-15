88362d046905aabc3134d0e37718d723b6758d73
Arike115 <ajibolaoluwafunke669@gmail.com>
2023-12-15 16:19:07 +0100

    changes

--- FirstCohortPractical/Program.cs
+++ FirstCohortPractical/Program.cs
@@ -9,40 +9,72 @@ public class Program
     //language-integrated query
     //query syntax or query expression
     //method syntax or method extension or fluent syntax
-    //Linqs have diferrent methods group inside the different categories
-    //filtering ===== where, offtype
-    //projection ==== select, select many 
+    //Linqs have different methods group inside the different categories
+   //Element Operators
+   //Aggregate Operators
+   //Quantifiers ====> any, all, contains
 
     private static void Main(string[] args)
     {
-        IList<Teacher> list = new List<Teacher>()
-        {
-            new Teacher() {Id = 1, Name = "Ada", Age = 98},
-            new Teacher() {Id = 2, Name = "Precious", Age = 28},
-            new Teacher() {Id = 3, Name = "Emeka", Age = 23},
-            new Teacher() {Id = 4, Name = "Grace", Age = 86},
-            new Teacher() {Id = 5, Name = "Ben", Age = 45},
-        };
+        //Quantifiers
+        string[] words = { "pen", "chalk", "duster", "desk", "jug", "mug" };
 
-        //query syntax
-        var adult = from s in list
-                    where s.Age > 28
-                    select s;
+        var anyresult = words.Any(s => s.Length > 4);
+        Console.WriteLine(anyresult);
 
-        //method syntax
-        var result = list.Where(s => s.Age > 28);
+        var allresult = words.All(s => s.Length > 4);
+        Console.WriteLine(allresult);
 
-        foreach(var item in adult )
-        {
-            Console.WriteLine(item.Name);
-        }
+        var contresult = words.Contains("chalk");
+        Console.WriteLine(contresult);
 
-        List<string> words = new List<string>() {"data","chalk","recharge"};
-        var data = words.Select(s => s);
-        foreach(var item in data )
-        {
-            Console.WriteLine(item);
-        }
+        //aggregate operators
+        //sum
+        //min
+        //max
+        //count
+        //average
+        //aggregate
+
+        int[] numeric = { 45, 6, 7, 8, 2, 2, 4, 90, 89, 45, 0, 23 };
+
+        var sum = numeric.Sum();
+        Console.WriteLine("sum = " + sum);
+        var min = numeric.Min();
+        Console.WriteLine("minimum = " +min);
+        var max = numeric.Max();
+        Console.WriteLine("maximum =" + max);
+        var count = numeric.Count();
+        Console.WriteLine("Count = " + count);
+        var avarage = numeric.Average();
+        Console.WriteLine("avaerage = " +avarage);
+
+
+        //element Operators
+        //first
+        //firstordefault
+        //last
+        //lastordefault
+        //single
+        //singleordefault
+
+        int[] numerics = { 45, 6, 7, 8, 2, 2, 4, 90, 89, 45, 0, 23 };
+        int[] numerics2 = { };
+
+        var v = numerics.First();
+        Console.WriteLine(v);
+        var y = numerics.FirstOrDefault();
+        Console.WriteLine(y);
+        var i = numerics2.FirstOrDefault();
+        Console.WriteLine(i);
+
+        var o = numerics.Last();
+        Console.WriteLine(o);
+        var n = numerics.LastOrDefault();
+        Console.WriteLine(n);
+        var z = numerics2.LastOrDefault();
+        Console.WriteLine(z);
+
     }
 
 
