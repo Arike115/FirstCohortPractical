0652d1e3d1ec25a80ebd8779a97790c3fb0778a3
Arike115 <ajibolaoluwafunke669@gmail.com>
2023-12-16 13:09:45 +0100

    changes

--- FirstCohortPractical/Program.cs
+++ FirstCohortPractical/Program.cs
@@ -20,7 +20,7 @@ public class Program
         double[] numbers1 = { 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.4, 2.1, 2.8 };
         double[] numbers2 = { 2.9, 2.4, 2.2, 2.10, 2.11, 23, 22 };
 
-        var result = numbers1.Distinct().ToList();
+        var result = numbers1.Distinct().ToArray();
         var data = numbers2.Except(numbers1).ToList();
         var intersect = numbers1.Intersect(numbers2).ToList();
         var union = numbers1.Union(numbers2).ToList();
@@ -30,6 +30,33 @@ public class Program
         }
 
 
+        IList<Teacher> list = new List<Teacher>()
+        {
+            new Teacher() {Id = 1, Name = "Ada", Age = 98},
+            new Teacher() {Id = 2, Name = "Precious", Age = 28},
+            new Teacher() {Id = 3, Name = "Emeka", Age = 23},
+            new Teacher() {Id = 3, Name = "Grace", Age = 86},
+            new Teacher() {Id = 4, Name = "Ben", Age = 28},
+        };
+
+        //Querysyntax
+
+        var orderbydatas = from s in list
+                          orderby s.Id
+                          select s;
+
+
+        var orderbydatadescen = from s in list
+                                orderby s.Id descending
+                                select s;
+
+        var orderbydata = list.OrderBy(x => x.Age).ThenBy(x => x.Name);
+
+
+        foreach(var v in orderbydata)
+        {
+            Console.WriteLine("the id = {0} and the Name = {1}",v.Id, v.Name);
+        }
     }
 
 
